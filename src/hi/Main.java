490a2bab72ba7fd8dcef71fd19eea65a07434b8d
UpayanDey <upayan10@gmail.com>
2022-05-30 19:46:48 +0530

    first branch 'issue1' commit

--- src/hi/Main.java
+++ src/hi/Main.java
@@ -1,10 +1,15 @@
 package hi;
 
+import hi_new.New2;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("HI");
 		System.out.println("No problem");
+		New2 ob=new New2();
+		System.out.println(ob.findlength("upayan"));
+		
 
 	}
 
---
+++ src/hi_new/New2.java
@@ -0,0 +1,10 @@
+package hi_new;
+
+public class New2 {
+	
+	public int findlength(String s)
+	{
+		return s.length();
+	}
+
+}
